c6351b8eb7990e832698a6a882ee30b55890eb23
annzb <annazabn@gmail.com>
2023-06-20 14:59:43 -0600

    Visualizable octomap update

--- src/octomap_postprocess.cpp
+++ src/octomap_postprocess.cpp
@@ -3,19 +3,82 @@
 #include <rosbag/view.h>
 #include <boost/foreach.hpp>
 
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <sensor_msgs/PointCloud2.h>
+
+#include <visualization_msgs/MarkerArray.h>
+
 #include <octomap/octomap.h>
 #include <octomap_msgs/conversions.h>
 #include <octomap_msgs/Octomap.h>
 
 #include "octree_diff.h" 
 
+
+std::pair<sensor_msgs::PointCloud2, visualization_msgs::MarkerArray> octreeToPointCloud2(const octomap::OcTree& tree, const std::string& frame_id, const ros::Time& stamp, double marker_lifetime = 0.1)
+{
+    // Extract occupied cells as an octomap::Pointcloud
+    pcl::PointCloud<pcl::PointXYZ> pclCloud;
+    visualization_msgs::MarkerArray marker_array;
+    size_t id = 0;
+
+    // Convert octomap::Pointcloud to pcl::PointCloud
+    
+    for(octomap::OcTree::leaf_iterator it = tree.begin_leafs(), end=tree.end_leafs(); it!= end; ++it) {
+        // If the node is occupied, add it to the point cloud.
+        if(tree.isNodeOccupied(*it)){
+            pclCloud.points.push_back(pcl::PointXYZ(it.getX(), it.getY(), it.getZ()));
+            
+	    // Create marker for each occupied node
+	    visualization_msgs::Marker marker;
+	    marker.lifetime = ros::Duration(marker_lifetime);
+	    marker.header.frame_id = frame_id;
+	    marker.header.stamp = stamp;
+	    marker.ns = "map";
+	    marker.id = id++;
+	    marker.type = visualization_msgs::Marker::CUBE;
+	    marker.action = visualization_msgs::Marker::ADD;
+	    marker.pose.position.x = it.getX();
+	    marker.pose.position.y = it.getY();
+	    marker.pose.position.z = it.getZ();
+	    marker.pose.orientation.x = 0.0;
+	    marker.pose.orientation.y = 0.0;
+	    marker.pose.orientation.z = 0.0;
+	    marker.pose.orientation.w = 1.0;
+	    marker.scale.x = tree.getNodeSize(it.getDepth());
+	    marker.scale.y = tree.getNodeSize(it.getDepth());
+	    marker.scale.z = tree.getNodeSize(it.getDepth());
+	    marker.color.a = 1.0;
+	    marker.color.r = 0.0;
+	    marker.color.g = 0.0;
+	    marker.color.b = 1.0;
+
+	    // Add marker to MarkerArray
+	    marker_array.markers.push_back(marker);
+	}
+    }
+
+    // Convert pcl::PointCloud to sensor_msgs::PointCloud2
+    sensor_msgs::PointCloud2 cloud;
+    pcl::toROSMsg(pclCloud, cloud);
+    cloud.header.frame_id = frame_id;
+    cloud.header.stamp = stamp;
+    
+    return std::make_pair(cloud, marker_array);
+}
+
+
 int main(int argc, char** argv) {
     ros::init(argc, argv, "octomap_postprocess_node");
     ros::NodeHandle nh("~");
 
     std::string input_bag_path, input_topic;
+    double marker_lifetime;
     nh.getParam("input_bag_path", input_bag_path);
     nh.getParam("input_topic", input_topic);
+    nh.getParam("marker_lifetime", marker_lifetime);
     
     std::string output_bag_path;
     std::string extension = ".bag";
@@ -47,13 +110,30 @@ int main(int argc, char** argv) {
         if (i_msg != nullptr) {
             octomap::OcTree* current_tree = dynamic_cast<octomap::OcTree*>(octomap_msgs::msgToMap(*i_msg));
             if (prev_tree != nullptr) {
+                std::string frame_id = i_msg->header.frame_id;
+                ros::Time stamp = i_msg->header.stamp;
+            
                 std::pair<octomap::OcTree, octomap::OcTree> diff_trees = calcOctreeDiff(*prev_tree, *current_tree);
                 octomap_msgs::Octomap diff_tree_msg, update_tree_msg;
-                octomap_msgs::binaryMapToMsg(diff_trees.second, diff_tree_msg);
                 octomap_msgs::binaryMapToMsg(diff_trees.first, update_tree_msg);
-
+                octomap_msgs::binaryMapToMsg(diff_trees.second, diff_tree_msg);
+                
+		 diff_tree_msg.header.frame_id = frame_id;
+		 diff_tree_msg.header.stamp = stamp;
+		 update_tree_msg.header.frame_id = frame_id;
+		 update_tree_msg.header.stamp = stamp;
+		 
+		 auto [update_pcl, update_array] = octreeToPointCloud2(diff_trees.first, frame_id, stamp, marker_lifetime);
+                auto [diff_pcl, diff_array] = octreeToPointCloud2(diff_trees.second, frame_id, stamp, marker_lifetime);
+                
+   		 output_bag.write(input_topic + "/update", msg.getTime(), update_tree_msg);
+                output_bag.write(input_topic + "/update/pcl", msg.getTime(), update_pcl);
+                output_bag.write(input_topic + "/update/array", msg.getTime(), update_array);
+                
                 output_bag.write(input_topic + "/diff", msg.getTime(), diff_tree_msg);
-                output_bag.write(input_topic + "/update", msg.getTime(), update_tree_msg);
+                output_bag.write(input_topic + "/diff/pcl", msg.getTime(), diff_pcl);
+                output_bag.write(input_topic + "/diff/array", msg.getTime(), diff_array);
+                
             }
             if (prev_tree != nullptr) delete prev_tree;
             prev_tree = current_tree;

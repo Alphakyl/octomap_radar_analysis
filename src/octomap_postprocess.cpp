24a685a7f8f68b39272b62ce5dbcf89af3cef62a
annadoxci <anna@doxci.ai>
2023-06-26 01:36:46 -0600

    Publish ego-centric PCL in postprocess node

--- src/octomap_postprocess.cpp
+++ src/octomap_postprocess.cpp
@@ -6,67 +6,93 @@
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <pcl_conversions/pcl_conversions.h>
-#include <sensor_msgs/PointCloud2.h>
 
+#include <geometry_msgs/Pose.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <nav_msgs/Odometry.h>
 #include <visualization_msgs/MarkerArray.h>
+
+#include <tf2_eigen/tf2_eigen.h>
 
 #include <octomap/octomap.h>
 #include <octomap_msgs/conversions.h>
 #include <octomap_msgs/Octomap.h>
 
-#include "octree_diff.h" 
+#include "octree_diff.h"
 
 
-std::pair<sensor_msgs::PointCloud2, visualization_msgs::MarkerArray> octreeToPointCloud2(const octomap::OcTree& tree, const std::string& frame_id, const ros::Time& stamp, double marker_lifetime = 0.1)
+Eigen::Affine3d poseMsgToEigen(const nav_msgs::Odometry::ConstPtr& msg)
 {
-    // Extract occupied cells as an octomap::Pointcloud
-    pcl::PointCloud<pcl::PointXYZ> pclCloud;
-    visualization_msgs::MarkerArray marker_array;
-    size_t id = 0;
+    geometry_msgs::Pose pose = msg->pose.pose;
+    geometry_msgs::Transform transform;
+    transform.translation.x = pose.position.x;
+    transform.translation.y = pose.position.y;
+    transform.translation.z = pose.position.z;
+    transform.rotation = pose.orientation;
+    Eigen::Affine3d eigenTransform = tf2::transformToEigen(transform);
+    return eigenTransform.inverse();
+}
 
-    // Convert octomap::Pointcloud to pcl::PointCloud
+
+std::tuple<sensor_msgs::PointCloud2, sensor_msgs::PointCloud2, visualization_msgs::MarkerArray> octreeToPointCloud2(
+    const octomap::OcTree& tree,
+    const std::string& frame_id,
+    const ros::Time& stamp,
+    const Eigen::Affine3d& center,
+    double marker_lifetime = 0.1
+) {
+    pcl::PointCloud<pcl::PointXYZ> pclCloudCentered;
+    pcl::PointCloud<pcl::PointXYZ> pclCloud;
+    visualization_msgs::MarkerArray markerArray;
+    size_t id = 0;
     
     for(octomap::OcTree::leaf_iterator it = tree.begin_leafs(), end=tree.end_leafs(); it!= end; ++it) {
-        // If the node is occupied, add it to the point cloud.
         if(tree.isNodeOccupied(*it)){
-            pclCloud.points.push_back(pcl::PointXYZ(it.getX(), it.getY(), it.getZ()));
-            
-	    // Create marker for each occupied node
-	    visualization_msgs::Marker marker;
-	    marker.lifetime = ros::Duration(marker_lifetime);
-	    marker.header.frame_id = frame_id;
-	    marker.header.stamp = stamp;
-	    marker.ns = "map";
-	    marker.id = id++;
-	    marker.type = visualization_msgs::Marker::CUBE;
-	    marker.action = visualization_msgs::Marker::ADD;
-	    marker.pose.position.x = it.getX();
-	    marker.pose.position.y = it.getY();
-	    marker.pose.position.z = it.getZ();
-	    marker.pose.orientation.x = 0.0;
-	    marker.pose.orientation.y = 0.0;
-	    marker.pose.orientation.z = 0.0;
-	    marker.pose.orientation.w = 1.0;
-	    marker.scale.x = tree.getNodeSize(it.getDepth());
-	    marker.scale.y = tree.getNodeSize(it.getDepth());
-	    marker.scale.z = tree.getNodeSize(it.getDepth());
-	    marker.color.a = 1.0;
-	    marker.color.r = 0.0;
-	    marker.color.g = 0.0;
-	    marker.color.b = 1.0;
+            Eigen::Vector3d pointOrigEig(it.getX(), it.getY(), it.getZ());
+            Eigen::Vector3d pointCenteredEig = center * pointOrigEig;
+            pcl::PointXYZ pointOrig(pointOrigEig[0], pointOrigEig[1], pointOrigEig[2]);
+            pcl::PointXYZ pointCentered(pointCenteredEig[0], pointCenteredEig[1], pointCenteredEig[2]);
 
-	    // Add marker to MarkerArray
-	    marker_array.markers.push_back(marker);
-	}
+            pclCloud.points.push_back(pointOrig);
+            pclCloudCentered.points.push_back(pointCentered);
+            
+            // Create marker for each occupied node
+            visualization_msgs::Marker marker;
+            marker.lifetime = ros::Duration(marker_lifetime);
+            marker.header.frame_id = frame_id;
+            marker.header.stamp = stamp;
+            marker.ns = "map";
+            marker.id = id++;
+            marker.type = visualization_msgs::Marker::CUBE;
+            marker.action = visualization_msgs::Marker::ADD;
+            marker.pose.position.x = pointOrig.x;
+            marker.pose.position.y = pointOrig.y;
+            marker.pose.position.z = pointOrig.z;
+            marker.pose.orientation.x = 0.0;
+            marker.pose.orientation.y = 0.0;
+            marker.pose.orientation.z = 0.0;
+            marker.pose.orientation.w = 1.0;
+            marker.scale.x = tree.getNodeSize(it.getDepth());
+            marker.scale.y = tree.getNodeSize(it.getDepth());
+            marker.scale.z = tree.getNodeSize(it.getDepth());
+            marker.color.a = 1.0;
+            marker.color.r = 0.0;
+            marker.color.g = 0.0;
+            marker.color.b = 1.0;
+            markerArray.markers.push_back(marker);
+        }
     }
 
-    // Convert pcl::PointCloud to sensor_msgs::PointCloud2
+    sensor_msgs::PointCloud2 cloudCentered;
     sensor_msgs::PointCloud2 cloud;
+    pcl::toROSMsg(pclCloudCentered, cloudCentered);
     pcl::toROSMsg(pclCloud, cloud);
+    cloudCentered.header.frame_id = frame_id;
+    cloudCentered.header.stamp = stamp;
     cloud.header.frame_id = frame_id;
     cloud.header.stamp = stamp;
     
-    return std::make_pair(cloud, marker_array);
+    return std::make_tuple(cloudCentered, cloud, markerArray);
 }
 
 
@@ -74,10 +100,11 @@ int main(int argc, char** argv) {
     ros::init(argc, argv, "octomap_postprocess_node");
     ros::NodeHandle nh("~");
 
-    std::string input_bag_path, input_topic;
+    std::string input_bag_path, input_topic, odometry_topic;
     double marker_lifetime;
     nh.getParam("input_bag_path", input_bag_path);
     nh.getParam("input_topic", input_topic);
+    nh.getParam("odometry_topic", odometry_topic);
     nh.getParam("marker_lifetime", marker_lifetime);
     
     std::string output_bag_path;
@@ -85,14 +112,11 @@ int main(int argc, char** argv) {
     if (input_bag_path.size() > extension.size() &&
         input_bag_path.substr(input_bag_path.size() - extension.size()) == extension)
     {
-        // If the input path ends with ".bag", replace it with "_diff.bag"
         output_bag_path = input_bag_path.substr(0, input_bag_path.size() - extension.size()) + "_diff.bag";
     } else {
-        // If it doesn't, just append "_diff.bag" to the input path
         output_bag_path = input_bag_path + "_diff.bag";
     }
 
-    
     rosbag::Bag input_bag, output_bag;
     try {
         input_bag.open(input_bag_path, rosbag::bagmode::Read);
@@ -101,36 +125,64 @@ int main(int argc, char** argv) {
         ROS_ERROR("Error opening bag files: %s", e.what());
         return 1;
     }
+    rosbag::View view(input_bag, rosbag::TopicQuery({input_topic, odometry_topic}));
 
-    rosbag::View view(input_bag, rosbag::TopicQuery(input_topic));
+    // Check if required topics exist
+    bool input_topic_found = false, odometry_topic_found = false;
+    BOOST_FOREACH(const rosbag::ConnectionInfo *info, view.getConnections()) {
+      if (info->topic == input_topic) {
+        input_topic_found = true;
+      }
+      else if (info->topic == odometry_topic) {
+        odometry_topic_found = true;
+      }
+    }
+    if (!input_topic_found) {
+        ROS_ERROR("Required topic '%s' not found in file '%s'", input_topic.c_str(), input_bag_path.c_str());
+        return 1;
+    }
+    if (!odometry_topic_found) {
+        ROS_ERROR("Required topic '%s' not found in file '%s'", odometry_topic.c_str(), input_bag_path.c_str());
+        return 1;
+    }
 
+    Eigen::Affine3d odometry_position = Eigen::Affine3d::Identity();
     octomap::OcTree* prev_tree = nullptr;
+
     BOOST_FOREACH(rosbag::MessageInstance const msg, view) {
         octomap_msgs::Octomap::ConstPtr i_msg = msg.instantiate<octomap_msgs::Octomap>();
+        nav_msgs::Odometry::ConstPtr o_msg = msg.instantiate<nav_msgs::Odometry>();
+
+        if (o_msg != nullptr) {
+            odometry_position = poseMsgToEigen(o_msg);
+        }
+
         if (i_msg != nullptr) {
             octomap::OcTree* current_tree = dynamic_cast<octomap::OcTree*>(octomap_msgs::msgToMap(*i_msg));
             if (prev_tree != nullptr) {
                 std::string frame_id = i_msg->header.frame_id;
                 ros::Time stamp = i_msg->header.stamp;
-            
+
                 std::pair<octomap::OcTree, octomap::OcTree> diff_trees = calcOctreeDiff(*prev_tree, *current_tree);
                 octomap_msgs::Octomap diff_tree_msg, update_tree_msg;
                 octomap_msgs::binaryMapToMsg(diff_trees.first, update_tree_msg);
                 octomap_msgs::binaryMapToMsg(diff_trees.second, diff_tree_msg);
                 
-		 diff_tree_msg.header.frame_id = frame_id;
-		 diff_tree_msg.header.stamp = stamp;
-		 update_tree_msg.header.frame_id = frame_id;
-		 update_tree_msg.header.stamp = stamp;
-		 
-		 auto [update_pcl, update_array] = octreeToPointCloud2(diff_trees.first, frame_id, stamp, marker_lifetime);
-                auto [diff_pcl, diff_array] = octreeToPointCloud2(diff_trees.second, frame_id, stamp, marker_lifetime);
-                
-   		 output_bag.write(input_topic + "/update", msg.getTime(), update_tree_msg);
+                diff_tree_msg.header.frame_id = frame_id;
+                diff_tree_msg.header.stamp = stamp;
+                update_tree_msg.header.frame_id = frame_id;
+                update_tree_msg.header.stamp = stamp;
+
+                auto [update_pcl_centered, update_pcl, update_array] = octreeToPointCloud2(diff_trees.first, frame_id, stamp, odometry_position, marker_lifetime);
+                auto [diff_pcl_centered, diff_pcl, diff_array] = octreeToPointCloud2(diff_trees.second, frame_id, stamp, odometry_position, marker_lifetime);
+
+                output_bag.write(input_topic + "/update", msg.getTime(), update_tree_msg);
+                output_bag.write(input_topic + "/update/pcl_centered", msg.getTime(), update_pcl_centered);
                 output_bag.write(input_topic + "/update/pcl", msg.getTime(), update_pcl);
                 output_bag.write(input_topic + "/update/array", msg.getTime(), update_array);
                 
                 output_bag.write(input_topic + "/diff", msg.getTime(), diff_tree_msg);
+                output_bag.write(input_topic + "/diff/pcl_centered", msg.getTime(), diff_pcl_centered);
                 output_bag.write(input_topic + "/diff/pcl", msg.getTime(), diff_pcl);
                 output_bag.write(input_topic + "/diff/array", msg.getTime(), diff_array);
                 
@@ -141,10 +193,8 @@ int main(int argc, char** argv) {
     }
 
     if (prev_tree != nullptr) delete prev_tree;
-
     input_bag.close();
     output_bag.close();
-
     return 0;
 }
 
